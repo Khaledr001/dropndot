6fda8c7810f174bbf95b5bdb506096b0821712d9
Khaledr001 <khaledrazu01@gmail.com>
2024-11-05 14:28:22 +0600

    env config updated

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,6 +1,5 @@
 import { config } from "dotenv";
 
-// Configure env file path
 config({
   path: `.env`,
   override: true,
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,8 @@ import { connectDB } from "./config/db";
 import { serverPort } from "./config/env";
 
 const startServer = async () => {
-  // Establish database connection
   await connectDB();
 
-  // Start the server
   app.listen(serverPort, () => {
     console.log(`Server is running on port ${serverPort}`);
   });
